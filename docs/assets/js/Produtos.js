052cb996e645f0e6b0bdf10e256ee67d317705ca
Daniel de Carvalho almeida <72422753+Danielzinho220@users.noreply.github.com>
2023-11-22 19:43:47 -0300

    22.11

---
+++ docs/assets/js/Produtos.js
@@ -0,0 +1,86 @@
+let ProductList = [
+    {
+      id: 1,
+      name: "Motor de partida",
+      img: "images/01.png",
+      prices: [560.0, 562.0, 563.89],
+      sizes: ["30", "40", "42"],
+      description:"Produto Original, importado dos EUA. 🤤",
+    },
+    {
+      id: 2,
+      name: "Eixo traseiro",
+      img: "images/jordan1mid.png",
+      prices: [500.0, 502.0, 503.50],
+      sizes: ["32", "40", "42"],
+      description: "Produto Original, importado dos EUA. 🤤",
+    },
+   
+    {
+      id: 3,
+      name: "Painel honda City",
+      img: "images/jordanPhat1.png",
+      prices: [710.0, 712.0, 713.45],
+      sizes: ["34", "41", "43"],
+      description:"Produto Original, importado dos EUA. 🤤",
+    },
+    {
+      id: 4,
+      name: "Volante retro",
+      img: "images/launch.png",
+      prices: [400.0, 403.0, 404.77],
+      sizes: ["32", "39", "43"],
+      description:"Produto Original, importado dos EUA. 🤤",
+    },
+    {
+      id: 5,
+      name: "Painel Volkswagen",
+      img: "images/oldLove.png",
+      prices: [610.0, 612.0, 614.43],
+      sizes: ["33", "40", "43"],
+      description:"Produto Original, importado dos EUA. 🤤",
+    },
+    {
+      id: 6,
+      name: "Kit 4 pneus aro 18",
+      img: "images/rebelxx.png",
+      prices: [720.50, 722.0, 724.0],
+      sizes: ["36", "41", "43"],
+      description:"Produto Original, importado dos EUA. 🤤",
+    },
+    {
+      id: 7,
+      name: "Kit rolamento",
+      img: "images/retroHigh.png",
+      prices: [550.0, 551.50, 553.0],
+      sizes: ["37", "40", "43"],
+      description:"Produto Original, importado dos EUA. 🤤",
+    },
+    {
+      id: 8,
+      name: "Kit multimídia",
+      img: "images/rebelxx.png",
+      prices: [720.50, 722.0, 724.0],
+      sizes: ["36", "41", "43"],
+      description:"Produto Original, importado dos EUA. 🤤",
+    },
+    {
+      id: 9,
+      name: "Filtro de carro",
+      img: "images/retroHigh.png",
+      prices: [550.0, 551.50, 553.0],
+      sizes: ["37", "40", "43"],
+      description:"Produto Original, importado dos EUA. 🤤",
+    },
+
+    {
+        id: 10,
+        name: "Pastilha de freio",
+        img: "images/retroHigh.png",
+        prices: [550.0, 551.50, 553.0],
+        sizes: ["37", "40", "43"],
+        description:"Produto Original, importado dos EUA. 🤤",
+      },
+  
+  ];
+  
